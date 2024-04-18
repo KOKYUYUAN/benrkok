07ce1b7d959142deae47301740605d4dafec747b
kok <b02210136@kok.com>
2024-04-18 10:08:25 +0800

    lab 2

--- app.js
+++ app.js
@@ -7,46 +7,56 @@ const port = process.env.PORT || 3000
 
 //e.g using for registration
 app.post('/register',async(req,res) => {
-    console.log(req.body);
-    //insertOne the registration data to mongo
-  
-    const hash = bcrypt.hashSync(req.body.password, 10);
-  
-    let resq = await client.db("testing").collection("file_1").insertOne({
-          name: req.body.username,
-          age: req.body.age,
-          gender: req.body.gender,
-          faculty: req.body.faculty,
-          password: hash //req.body.password
-      });
-      res.send(resq);
+  let EXISTS = await
+  client.db("benr24231").collection("datacollection").findOne({
+    username: req.body.username
   })
-
-  //DELETE USER FROM DATABASE
-  //app.delete('/register',async(req,res) => {
-  //})
-
-  //update user info
-  //app.patch('/register',async(req,res) => {
-  //})
+  if(EXISTS){
+    res.status(404).send('User already exists')
+    return
+  } 
+  //console.log(req.body.username);
+  //insertOne the registration data to mongo
+  else{
+  const hash = bcrypt.hashSync(req.body.password, 10);
   
-  
-  app.post('/login',async(req,res) => { 
-        let resp = await client.db("testing").collection("file_1").findOne({
-          username: req.body.username
+  let result = await client.db("benr24231").collection("datacollection").insertOne({
+    username: req.body.username,
+    password: hash,
+    name: req.body.name,
+    email: req.body.email
   })
-    console.log(resp);
-    console.log(req.body);
+  res.send(result)
+}
+})
+
+app.post('/login',async(req,res) => { 
+// step #1:req.body.username
+  let result = await client.db("benr24231").collection("datacollection").findOne({
+    username: req.body.username
+})
+console.log(result);
+console.log(req.body);
+if(!req.body.username || !req.body.password){
+  res.status(404).send('Please provide username and password')
+}
+else if(req.body.username != null && req.body.password != null){
+
+  if(result){
+    //step2:if user exists, check if password is correct
+    if(bcrypt.compareSync(req.body.password,result.password)==true){
+      //paaword is correct
+      res.send('Login successful');
+    } else{
+      //password is incorrect
+      res.status(404).send('Wrong Password');
+    
+    }
+  }else{
+    //step3:if user not found
+    res.send('User not found');
   
-        if(!resp){
-          res.send('User not found');
-        }else{
-          if(bcrypt.compareSync(req.body.password,resp.password)==true){
-            res.send('Login successful');
-          } else{
-            res.send('Wrong Password');
-          
-          }
-        }
-        
-  });
+  }
+}
+
+});
--- index.js
+++ index.js
@@ -14,10 +14,19 @@ app.listen(port, () => {
 })
 
 app.post('/register',async(req,res) => {
+  let EXISTS = await
+  client.db("benr24231").collection("datacollection").findOne({
+    username: req.body.username
+  })
+  if(EXISTS){
+    res.status(404).send('User already exists')
+    return
+  } 
   //console.log(req.body.username);
   //insertOne the registration data to mongo
+  else{
   const hash = bcrypt.hashSync(req.body.password, 10);
-
+  
   let result = await client.db("benr24231").collection("datacollection").insertOne({
     username: req.body.username,
     password: hash,
@@ -25,40 +34,38 @@ app.post('/register',async(req,res) => {
     email: req.body.email
   })
   res.send(result)
+}
 })
 
 app.post('/login',async(req,res) => { 
-// step #1:req.body.username
-  let result = await client.db("benr24231").collection("datacollection").findOne({
-    username: req.body.username
-})
-console.log(result);
-console.log(req.body);
-
-if(req.body.username != null && req.body.password != null){
-
-  if(result){
-    //step2:if user exists, check if password is correct
-    if(bcrypt.compareSync(req.body.password,result.password)==true){
-      //paaword is correct
-      res.send('Login successful');
-    } else{
-      //password is incorrect
-      res.status(404).send('Wrong Password');
+  // step #1:req.body.username
+    let result = await client.db("benr24231").collection("datacollection").findOne({
+      username: req.body.username
+  })
+  console.log(result);
+  console.log(req.body);
+  if(!req.body.username || !req.body.password){
+    res.status(404).send('Please provide username and password')
+  }
+  else if(req.body.username != null && req.body.password != null){
+  
+    if(result){
+      //step2:if user exists, check if password is correct
+      if(bcrypt.compareSync(req.body.password,result.password)==true){
+        //paaword is correct
+        res.send('Login successful');
+      } else{
+        //password is incorrect
+        res.status(404).send('Wrong Password');
+      
+      }
+    }else{
+      //step3:if user not found
+      res.send('User not found');
     
     }
-  }else{
-    //step3:if user not found
-    res.send('User not found');
-  
-  }
 }
-else{
-     res.status(404).send('Please enter username and password');
-
-}
-});
-
+})
 
 
 
